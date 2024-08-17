c5efd7f1361844a2503c0c44300fabffb296a911
Thibeau Duerinck <thibeau.duerinck@icloud.com>
2024-08-17 16:32:30 +0200

    Add GameManager

---
+++ Core/Managers/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameDev.Core.Managers
+{
+    public enum GameState
+    {
+        StartScreen,
+        Playing
+    }
+
+    public class GameManager
+    {
+        public GameState CurrentGameState { get; private set; } = GameState.StartScreen;
+
+        private Texture2D _startScreenBackground;
+        private Texture2D _startButtonTexture;
+        private Texture2D _mapBackground;
+        private Rectangle _startButtonRectangle;
+
+        public void LoadContent(Texture2D startScreenBackground, Texture2D startButtonTexture, Texture2D mapBackground, GraphicsDevice graphicsDevice)
+        {
+            _startScreenBackground = startScreenBackground;
+            _startButtonTexture = startButtonTexture;
+            _mapBackground = mapBackground;
+
+            int buttonWidth = _startButtonTexture.Width;
+            int buttonHeight = _startButtonTexture.Height;
+            int buttonX = (graphicsDevice.Viewport.Width - buttonWidth) / 2;
+            int buttonY = (graphicsDevice.Viewport.Height - buttonHeight) / 2 + buttonHeight;
+
+            _startButtonRectangle = new Rectangle(buttonX, buttonY, buttonWidth, buttonHeight);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (CurrentGameState == GameState.StartScreen)
+            {
+                MouseState mouseState = Mouse.GetState();
+                if (mouseState.LeftButton == ButtonState.Pressed && _startButtonRectangle.Contains(mouseState.Position))
+                {
+                    StartGame();
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (CurrentGameState == GameState.StartScreen)
+            {
+                spriteBatch.Draw(_startScreenBackground, Vector2.Zero, Color.White);
+                spriteBatch.Draw(_startButtonTexture, _startButtonRectangle, Color.White);
+            }
+            else if (CurrentGameState == GameState.Playing)
+            {
+                spriteBatch.Draw(_mapBackground, Vector2.Zero, Color.White);
+            }
+        }
+
+        private void StartGame()
+        {
+            CurrentGameState = GameState.Playing;
+            _startScreenBackground = null;
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -5,7 +5,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
-using System.Reflection.Metadata;
 using TiledSharp;
 
 namespace GameDev
@@ -17,8 +16,11 @@ namespace GameDev
 
         private Texture2D _runTexture;
         private Texture2D _idleTexture;
-        private Texture2D _backgroundTexture;
+        private Texture2D _backgroundTexture; 
+        private Texture2D _startButtonTexture;
+        private Texture2D _mapBackground; 
         private Wizard _wizard;
+        private GameManager _gameManager;
 
         private TmxMap _map;
         private MapManager _mapManager;
@@ -32,6 +34,7 @@ namespace GameDev
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _gameManager = new GameManager();
         }
 
         protected override void Initialize()
@@ -41,67 +44,83 @@ namespace GameDev
 
         protected override void LoadContent()
         {
+            SetFullScreen();
 
-        SetFullScreen();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _backgroundTexture = Content.Load<Texture2D>("UI/StartScreen");
+            _startButtonTexture = Content.Load<Texture2D>("UI/Startbutton");
+            _mapBackground = Content.Load<Texture2D>("Tiles/World1");
 
-        _backgroundTexture = Content.Load<Texture2D>("Tiles/World1");
-        _map = new TmxMap("Content/Maps/Level_1.tmx");
-        _tilesetTexture = Content.Load<Texture2D>("Tiles/Assets");
-        var tileWidth = _map.Tilesets[0].TileWidth;
-        var tileHeight = _map.Tilesets[0].TileHeight;
-        var tileSetTilesWide = _tilesetTexture.Width / tileWidth;
-        _mapManager = new MapManager(_spriteBatch, _map, _tilesetTexture, tileSetTilesWide, tileWidth, tileHeight);
+            _map = new TmxMap("Content/Maps/Level_1.tmx");
+            _tilesetTexture = Content.Load<Texture2D>("Tiles/Assets");
+            var tileWidth = _map.Tilesets[0].TileWidth;
+            var tileHeight = _map.Tilesets[0].TileHeight;
+            var tileSetTilesWide = _tilesetTexture.Width / tileWidth;
+            _mapManager = new MapManager(_spriteBatch, _map, _tilesetTexture, tileSetTilesWide, tileWidth, tileHeight);
 
-        _idleTexture = Content.Load<Texture2D>("Wizard/Idle-Sheet");
-        _runTexture = Content.Load<Texture2D>("Wizard/Run-Sheet");
+            _idleTexture = Content.Load<Texture2D>("Wizard/Idle-Sheet");
+            _runTexture = Content.Load<Texture2D>("Wizard/Run-Sheet");
 
-        InitializeGameObjects();
+            InitializeGameObjects();
+
+            _gameManager.LoadContent(_backgroundTexture, _startButtonTexture, _mapBackground, GraphicsDevice);
         }
 
         private void InitializeGameObjects()
         {
-        _wizard = new Wizard(_runTexture, _idleTexture, new KeyboardReader(), new MovementManager(), _mapManager);
+            _wizard = new Wizard(_runTexture, _idleTexture, new KeyboardReader(), new MovementManager(), _mapManager);
         }
 
         protected override void Update(GameTime gameTime)
         {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
 
-        _wizard.Update(gameTime);
-        base.Update(gameTime);
+            _gameManager.Update(gameTime);
+
+            if (_gameManager.CurrentGameState == GameState.Playing)
+            {
+                _wizard.Update(gameTime);
+            }
+
+            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-        _spriteBatch.Begin(transformMatrix: _transformMatrix);
-        _spriteBatch.Draw(_backgroundTexture, Vector2.Zero, Color.White);
-        _mapManager.Draw(_spriteBatch);
-        _wizard.Draw(_spriteBatch);
-        _spriteBatch.End();
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        base.Draw(gameTime);
+            _spriteBatch.Begin(transformMatrix: _gameManager.CurrentGameState == GameState.Playing ? _transformMatrix : Matrix.Identity);
+
+            _gameManager.Draw(_spriteBatch);
+
+            if (_gameManager.CurrentGameState == GameState.Playing)
+            {
+                _mapManager.Draw(_spriteBatch);
+                _wizard.Draw(_spriteBatch);
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
         }
 
         private void SetFullScreen()
         {
-        _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-        _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-        _graphics.IsFullScreen = true;
-        _graphics.ApplyChanges();
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
 
-        
-        int screenHeight = GraphicsDevice.Viewport.Height;
-        int screenWidth = GraphicsDevice.Viewport.Width;
+            int screenHeight = GraphicsDevice.Viewport.Height;
+            int screenWidth = GraphicsDevice.Viewport.Width;
 
-        int mapWidth = 800;
-        int mapHeight = 480;
+            int mapWidth = 800;
+            int mapHeight = 480;
 
-        _scale = Math.Min(screenWidth / (float)mapWidth, screenHeight / (float)mapHeight);
-        _transformMatrix = Matrix.CreateScale(_scale);
+            _scale = Math.Min(screenWidth / (float)mapWidth, screenHeight / (float)mapHeight);
+            _transformMatrix = Matrix.CreateScale(_scale);
         }
     }
 }
